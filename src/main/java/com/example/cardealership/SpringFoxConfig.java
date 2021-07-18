9b10483f7e3a7e5edb45dac12ef7586d7be2cec1
Giovanni <gvn.gentile@gmail.com>
2021-07-18 15:31:41 -0300

    Adding swagger generator and README.md

---
+++ src/main/java/com/example/cardealership/SpringFoxConfig.java
@@ -0,0 +1,35 @@
+package com.example.cardealership;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.builders.ApiInfoBuilder;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Car Dealership REST API")
+                .description("\"Implements a Car Dealership API that allows to register, find and maintain"
+                        + "vehicles. It also generate reports about selling and maintenances.\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .build();
+    }
+}
