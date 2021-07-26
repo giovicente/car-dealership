202504d2df0e318d302a1307fbdb1cf69d82e960
Giovanni <gvn.gentile@gmail.com>
2021-07-26 19:43:53 -0300

    feat: add passenger car search by manufacturer or model

--- src/main/java/com/example/cardealership/passengercar/PassengerCarRepository.java
+++ src/main/java/com/example/cardealership/passengercar/PassengerCarRepository.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 
 public interface PassengerCarRepository extends CrudRepository<PassengerCar, Long> {
     Optional<PassengerCar> findOneByLicensePlateIgnoreCase(String licensePLate);
+    Iterable<PassengerCar> findAllByManufacturerOrModel(String manufacturer, String model);
 }
